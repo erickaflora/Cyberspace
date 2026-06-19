d6390d1e4a948fde3e04f0960260ae8ac4219e2d
Ericka Rudico <erickarudico@gmail.com>
2026-06-19 17:34:26 +0800

    replaced vue with react.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
+  }
 })
